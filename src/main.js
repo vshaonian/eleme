b8342b96f8d3fbb690ead13c30c7baa9169115c5
vshaonian <1251602300@qq.com>
2017-03-22 22:35:04 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+import VueRouter from './router';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
 
 /* eslint-disable no-new */
 new Vue({
@@ -11,3 +14,20 @@ new Vue({
   template: '<App/>',
   components: {App}
 });
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: 'seller', component: seller}
+];
+
+const router = new VueRouter({
+  routes
+});
+
+const app = new Vue({
+  router
+}).$mount('#app');
+export default app;

6670512db60479d75a206898ba0a4bfc5e18a128
vshaonian <1251602300@qq.com>
2017-03-24 15:42:53 +0800

    设置activeClass

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <router-view name="Header"></router-view>
     <div class="tab">
       <div class="tab-item">
-        <router-link to="/goods">商品</router-link>
+        <router-link to="/goods" activeClass="active">商品</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/ratings">评价</router-link>
+        <router-link to="/ratings" activeClass="active">评价</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/seller">商家</router-link>
+        <router-link to="/seller" activeClass="active">商家</router-link>
       </div>
     </div>
     <router-view></router-view>
@@ -30,4 +30,10 @@
       .tab-item
         flex: 1
         text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240, 20, 20)
 </style>

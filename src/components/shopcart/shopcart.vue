118b58e2bac03138da5c4ab8c382eb2335d1c886
vshaonian <1251602300@qq.com>
2017-04-09 15:46:46 +0800

    购物小球动画

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -17,7 +17,8 @@
         </div>
       </div>
       <div class="ball-container">
-        <transition v-for="ball in balls" name="drop">
+        <transition v-for="ball in balls" name="drop" @before-enter="beforeDrop" @enter="dropping"
+                    @after-enter="afterDrop">
           <div class="ball" v-show="ball.show">
             <div class="inner inner-hook"></div>
           </div>
@@ -37,7 +38,7 @@
                   <span>￥{{food.price * food.count}}</span>
                 </div>
                 <div class="cartcontrol-wrapper">
-                  <cartcontrol :food="food"></cartcontrol>
+                  <cartcontrol @add="addFood" :food="food"></cartcontrol>
                 </div>
               </li>
             </ul>
@@ -91,6 +92,7 @@
             show: false
           }
         ],
+        dropBalls: [],
         fold: true
       };
     },
@@ -148,7 +150,51 @@
     },
     methods: {
       drop(el) {
-        console.log(el);
+        for (let i = 0; i < this.balls.length; i++) {
+          let ball = this.balls[i];
+          if (!ball.show) {
+            ball.show = true;
+            ball.el = el;
+            this.dropBalls.push(ball);
+            return;
+          }
+        }
+      },
+      beforeDrop(el) {
+        let count = this.balls.length;
+        while (count--) {
+          let ball = this.balls[count];
+          if (ball.show) {
+            let rect = ball.el.getBoundingClientRect();
+            let x = rect.left - 32;
+            let y = -(window.innerHeight - rect.top - 22);
+            el.style.display = '';
+            el.style.webkitTransform = `translate3d(0,${y}px,0)`;
+            el.style.transform = `translate3d(0,${y}px,0)`;
+            let inner = el.getElementsByClassName('inner-hook')[0];
+            inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+            inner.style.transform = `translate3d(${x}px,0,0)`;
+          }
+        }
+      },
+      dropping(el) {
+        /* eslint-disable no-unused-vars */
+        let rf = el.offsetHeight;
+        this.$nextTick(() => {
+          el.style.webkitTransform = 'translate3d(0,0,0)';
+          el.style.transform = 'translate3d(0,0,0)';
+          let inner = el.getElementsByClassName('inner-hook')[0];
+          inner.style.webkitTransform = 'translate3d(0,0,0)';
+          inner.style.transform = 'translate3d(0,0,0)';
+//          el.addEventListener('transitioned', done);
+        });
+      },
+      afterDrop(el) {
+        let ball = this.dropBalls.shift();
+        if (ball) {
+          ball.show = false;
+          el.style.display = 'none';
+        }
       },
       toggleList() {
         if (!this.totalCount) {
@@ -169,6 +215,9 @@
           return;
         }
         window.alert(`支付${this.totalPrice}元`);
+      },
+      addFood(target) {
+        this.drop(target);
       }
     },
     components: {
@@ -273,13 +322,13 @@
         left: 32px
         bottom: 22px
         z-index: 200
-        transition: all 0.4s
+        transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
         .inner
           width: 16px
           height: 16px
           border-radius: 50%
           background: rgb(0, 160, 220)
-          transition: all 0.4s
+          transition: all 0.4s linear
     .shopcart-list
       position: absolute
       left: 0

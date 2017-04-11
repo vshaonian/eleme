916b187e5b4b099b53cd20f374a35513f298d68f
vshaonian <1251602300@qq.com>
2017-04-11 14:58:27 +0800

    详情页购物车

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,19 +1,39 @@
 <template>
-    <transition name="move">
-      <div v-show="showFlag" class="food">
-        <div class="food-content">
-          <div class="image-header">
-            <img :src="food.image" alt="">
-            <div class="back" @click="hide">
-              <i class="icon-arrow_lift"></i>
-            </div>
+  <transition name="move">
+    <div v-show="showFlag" class="food" ref="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image" alt="">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
           </div>
         </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <transition name="fade">
+          <div class="buy" v-show="!food.count || food.count===0" @click="addFirst">加入购物车</div>
+        </transition>
       </div>
-    </transition>
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  import Vue from 'vue';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
+
   export default {
     props: {
       food: {
@@ -28,10 +48,29 @@
     methods: {
       show() {
         this.showFlag = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
       },
       hide() {
         this.showFlag = false;
+      },
+      addFirst(event) {
+        if (!event._constructed) {
+          return;
+        }
+        Vue.set(this.food, 'count', 1);
+        this.$emit('add', event.target);
       }
+    },
+    components: {
+      cartcontrol
     }
   };
 </script>
@@ -69,4 +108,56 @@
           padding: 10px
           font-size: 20px
           color: #fff
+    .content
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 154, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 18px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
+
+    .cartcontrol-wrapper
+      position: absolute
+      right: 12px
+      bottom: 12px
+    .buy
+      position: absolute
+      right: 18px
+      bottom: 18px
+      z-index: 10px
+      height: 24px
+      line-height: 24px
+      padding: 0 12px
+      box-sizing: border-box
+      font-size: 10px
+      border-radius: 12px
+      color: #fff
+      background: rgb(0, 160, 220)
+      &.fade-enter-active, &.fade-leave-active
+        transition: all 0.2s
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+        z-index: -1
 </style>
